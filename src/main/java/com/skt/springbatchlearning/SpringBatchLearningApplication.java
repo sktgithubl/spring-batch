8092ad9e6332a0d5fa0b8dccda451ebb0916578a
sktgithubl <skt.sipun123@gmail.com>
2021-09-21 14:54:53 +0530

    initial commit

---
+++ src/main/java/com/skt/springbatchlearning/SpringBatchLearningApplication.java
@@ -0,0 +1,14 @@
+package com.skt.springbatchlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLearningApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(SpringBatchLearningApplication.class, args);
+		System.exit(SpringApplication.exit(SpringApplication.run(SpringBatchLearningApplication.class)));
+	}
+
+}
---
+++ src/test/java/com/skt/springbatchlearning/SpringBatchLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skt.springbatchlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
